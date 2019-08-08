5b41a6be7c5be366675b33d0247ec8999589fc15
Matthew Phillips <phillipsm2@gmail.com>
2019-08-08 07:54:29 -0400

    Add a test about state changes

--- packages/xstate/test/test.js
+++ packages/xstate/test/test.js
@@ -1,4 +1,11 @@
 import { machine } from '../dist/lucy-xstate.js';
+import { interpret } from '../../../node_modules/xstate/dist/xstate.web.js';
+
+function withService(machine, cb) {
+  const service = interpret(machine).start();
+  cb(service);
+  service.stop();
+}
 
 QUnit.module('@lucy/xstate');
 
@@ -10,4 +17,24 @@ QUnit.test('Creates a machine', assert => {
   `;
 
   assert.ok(helloMachine, 'Created a machine');
+});
+
+QUnit.test('Can change simple states', assert => {
+  let toggleMachine = machine`
+    initial state enabled {
+      toggle => disabled
+    }
+
+    state disabled {
+      toggle => enabled
+    }
+  `;
+
+  withService(toggleMachine, service => {
+    assert.equal(service.state.value, 'enabled');
+    service.send('toggle');
+    assert.equal(service.state.value, 'disabled');
+    service.send('toggle');
+    assert.equal(service.state.value, 'enabled');
+  });
 });
