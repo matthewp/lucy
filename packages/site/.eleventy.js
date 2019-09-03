6396e8067ed910cae64d759b343de5e887ecf334
Matthew Phillips <phillipsm2@gmail.com>
2019-09-02 20:01:56 -0400

    Using <base href>

--- packages/site/.eleventy.js
+++ packages/site/.eleventy.js
@@ -1,12 +1,13 @@
+const path = require('path');
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const lucyPrism = require('@lucy/prism');
 const markdownIt = require('markdown-it');
 const markdownItTocAndAnchor = require('markdown-it-toc-and-anchor').default;
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.setTemplateFormats([ "md" ]);
-  eleventyConfig.addPassthroughCopy("styles");
-  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.setTemplateFormats([ 'md' ]);
+  eleventyConfig.addPassthroughCopy('styles');
+  eleventyConfig.addPassthroughCopy('images');
 
   eleventyConfig.addPlugin(pluginSyntaxHighlight, {
     init({ Prism }) {
@@ -21,4 +22,13 @@ module.exports = function(eleventyConfig) {
     anchorClassName: 'heading-anchor',
     tocClassName: 'toc'
   }));
+
+  eleventyConfig.addFilter('bareUrl', url => {
+    return url.substr(1);
+  });
+
+  eleventyConfig.addFilter('baseUrl', url => {
+    let rel = path.relative(path.dirname(url), '/') || '.';
+    return rel + '/';
+  });
 };
